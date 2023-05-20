872ead5650f64bb4c33aa30da30ccd5624a825cd
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2023-05-19 21:48:19 -0700

    Landing page created

--- portfolio-v2/src/App.tsx
+++ portfolio-v2/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <LandingPage />
     </div>
   );
 }
---
+++ portfolio-v2/src/components/LandingPage.tsx
@@ -0,0 +1,5 @@
+import styles from '../styles/LandingPage.module.css'
+
+export default function() {
+    return <div className={styles.LandingPageContainer}>Testing</div>
+}
